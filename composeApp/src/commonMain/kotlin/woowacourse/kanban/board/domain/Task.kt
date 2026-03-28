ddac0d10ce95df5e2ed5aa65578f396003d0726f
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-28 10:03:43 +0900

    refactor: UUID를 적용하여 Task를 생성

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Task.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/domain/Task.kt
@@ -1,14 +1,12 @@
 package woowacourse.kanban.board.domain
 
+import java.util.UUID
+
 data class Task(
-    val id: Int = ++idCounter,
+    val id: UUID = UUID.randomUUID(),
     val title: String,
     val content: String = "",
     val tags: List<String> = emptyList(),
     val taskState: TaskState = TaskState.TO_DO,
     val author: String = "다이노",
-) {
-    companion object {
-        private var idCounter = 0
-    }
-}
+)
