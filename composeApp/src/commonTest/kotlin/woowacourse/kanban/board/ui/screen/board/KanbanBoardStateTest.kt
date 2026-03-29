448cf16bed0642c392d646e1a56a83b683ef99e8
todays_sun_day <178suna@gmail.com>
2026-03-29 15:06:09 +0900

    test: KanbanBoardState의 테스트 추가로 작성

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/screen/board/KanbanBoardStateTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/screen/board/KanbanBoardStateTest.kt
@@ -62,4 +62,71 @@ class KanbanBoardStateTest {
         // Then: tasks 내부에 IN_PROGRESS 인 태스크가 한 개 존재한다.
         assertThat(kanbanBoardState.tasks.count { it.status == Status.IN_PROGRESS }).isEqualTo(1)
     }
+
+    @Test
+    fun `getProjectsTitles()로 프로젝트의 제목들을 불러온다`() {
+        // Given: 프로젝트 1번, 2번의 이름을 가진 칸반보드를 생성한다.
+        val kanbanBoard = KanbanBoard()
+        val projects = listOf(
+            KanbanProject(title = "프로젝트 1번"),
+            KanbanProject(title = "프로젝트 2번"),
+        )
+        val kanbanBoardState = KanbanBoardState(kanbanBoard, projects)
+
+        // When: 프로젝트의 제목들을 불러온다.
+        val result = kanbanBoardState.getProjectsTitles()
+
+        // Then: 불러온 결과값의 개수가 2개이고, 각각 '프로젝트 1번', '프로젝트 2번'을 포함한다.
+        assertThat(result.count()).isEqualTo(2)
+        assertThat(result).contains("프로젝트 1번")
+        assertThat(result).contains("프로젝트 2번")
+    }
+
+    @Test
+    fun `getCompleteCount()로 완료된 태스크의 개수를 셀 수 있다`() {
+        // Given: 프로젝트 1번에는 DONE 0개, 프로젝트 2번에는 DONE 1개를 생성한다.
+        val task1 = createKanbanTask(id = 0L, status = Status.IN_PROGRESS)
+        val task2 = createKanbanTask(id = 1L, status = Status.IN_PROGRESS)
+        val task3 = createKanbanTask(id = 2L, status = Status.DONE)
+        val kanbanBoard = KanbanBoard(listOf(task1, task2, task3))
+        val projects = listOf(
+            KanbanProject(title = "프로젝트 1번", taskIds = listOf(0L)),
+            KanbanProject(title = "프로젝트 2번", taskIds = listOf(1L, 2L)),
+        )
+        val kanbanBoardState = KanbanBoardState(kanbanBoard, projects)
+
+        // When: result1 에는 프로젝트 1의 DONE의 개수를, result2는 프로젝트 2의 DONE의 개수를 불러온다.
+        kanbanBoardState.updateSelectedProjectIndex(0)
+        val result1 = kanbanBoardState.getCompleteCount()
+        kanbanBoardState.updateSelectedProjectIndex(1)
+        val result2 = kanbanBoardState.getCompleteCount()
+
+        // Then: result1 에는 DONE이 0개, result2에는 DONE이 1개 뜬다.
+        assertThat(result1).isEqualTo(0)
+        assertThat(result2).isEqualTo(1)
+    }
+
+    @Test
+    fun `getTotalCount()로 총 태스크의 개수를 불러올 수 있다`() {
+        // Given: 프로젝트 1번에는 Task 1개, 프로젝트 2번에는 Task 2개를 생성한다.
+        val task1 = createKanbanTask(id = 0L, status = Status.TO_DO)
+        val task2 = createKanbanTask(id = 1L, status = Status.IN_PROGRESS)
+        val task3 = createKanbanTask(id = 2L, status = Status.DONE)
+        val kanbanBoard = KanbanBoard(listOf(task1, task2, task3))
+        val projects = listOf(
+            KanbanProject(title = "프로젝트 1번", taskIds = listOf(0L)),
+            KanbanProject(title = "프로젝트 2번", taskIds = listOf(1L, 2L)),
+        )
+        val kanbanBoardState = KanbanBoardState(kanbanBoard, projects)
+
+        // When: result1에는 프로젝트 1의 Task의 개수를, result2는 프로젝트 2의 Task의 개수를 불러온다.
+        kanbanBoardState.updateSelectedProjectIndex(0)
+        val result1 = kanbanBoardState.getTotalCount()
+        kanbanBoardState.updateSelectedProjectIndex(1)
+        val result2 = kanbanBoardState.getTotalCount()
+
+        // Then: result1에는 Task 1개, result2에는 Task 2개가 뜬다.
+        assertThat(result1).isEqualTo(1)
+        assertThat(result2).isEqualTo(2)
+    }
 }
