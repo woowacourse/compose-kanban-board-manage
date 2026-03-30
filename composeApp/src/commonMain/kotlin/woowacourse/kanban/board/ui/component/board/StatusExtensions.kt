1b43af027ee60e2249fb0ef8e07494a120079634
todays_sun_day <178suna@gmail.com>
2026-03-31 03:12:13 +0900

    refactor: Status를 String으로 반환하는 과정에서 strings.xml 이용

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/component/board/StatusExtensions.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/component/board/StatusExtensions.kt
@@ -1,6 +1,12 @@
 package woowacourse.kanban.board.ui.component.board
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import kanbanboard.composeapp.generated.resources.Res
+import kanbanboard.composeapp.generated.resources.status_done
+import kanbanboard.composeapp.generated.resources.status_in_progress
+import kanbanboard.composeapp.generated.resources.status_to_do
+import org.jetbrains.compose.resources.stringResource
 import woowacourse.kanban.board.domain.dialog.Status
 
 fun Status.toMainColor(): Color = when (this) {
@@ -21,8 +27,9 @@ fun Status.toBodyColor(): Color = when (this) {
     Status.DONE -> Color(0xFFF0FDF4)
 }
 
+@Composable
 fun Status.toTitle(): String = when (this) {
-    Status.TO_DO -> "To Do"
-    Status.IN_PROGRESS -> "In Progress"
-    Status.DONE -> "Done"
+    Status.TO_DO -> stringResource(Res.string.status_to_do)
+    Status.IN_PROGRESS -> stringResource(Res.string.status_in_progress)
+    Status.DONE -> stringResource(Res.string.status_done)
 }
