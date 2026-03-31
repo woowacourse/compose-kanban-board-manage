deb8e51ef7d234dac9d11793d7a6d6ddf9031cfa
ujeong <qnf323@naver.com>
2026-03-31 18:45:15 +0900

    test: Review, Done 상태인 태스크를 삭제하려고 하면 실패한다

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/domain/KanbanProjectTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/domain/KanbanProjectTest.kt
@@ -51,4 +51,42 @@ class KanbanProjectTest {
 
         assertEquals(TaskStatus.DONE, state.getTasksByStatus(TaskStatus.DONE).first().status)
     }
+
+    @Test
+    fun `Review, Done 상태인 태스크를 삭제하려고 하면 실패한다`() = runTest {
+        // given : Review, Done 상태의 태스크목록을 가진 프로젝트와 스테이트홀더가 제공된다
+        val tasks = listOf<KanbanTask>(
+            KanbanTask(
+                data = BoardData(
+                    title = Title("제목"),
+                    content = "내용",
+                    tags = Tags(),
+                    nickname = Nickname("아오"),
+                    id = 0,
+                ),
+                status = TaskStatus.DONE,
+            ),
+            KanbanTask(
+                data = BoardData(
+                    title = Title("제목"),
+                    content = "내용",
+                    tags = Tags(),
+                    nickname = Nickname("아오"),
+                    id = 1,
+                ),
+                status = TaskStatus.REVIEW,
+            ),
+        )
+        val project = KanbanProject(tasks)
+        val state = BoardState(project)
+        // when : 각  태스크를 지울 때
+
+        val resultDone: Boolean = state.deleteTask(0)
+
+        val resultReview: Boolean = state.deleteTask(1)
+
+        // then : 삭제가 실패해야 한다
+        assertEquals(false, resultDone)
+        assertEquals(false, resultReview)
+    }
 }
