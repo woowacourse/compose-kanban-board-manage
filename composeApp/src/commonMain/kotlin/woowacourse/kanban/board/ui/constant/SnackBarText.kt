72fda62180d53afbffc726957388dcf11e6ef6c7
ujeong <qnf323@naver.com>
2026-04-01 17:56:26 +0900

    fix: 스낵바 텍스트 수정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/constant/SnackBarText.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/constant/SnackBarText.kt
@@ -3,8 +3,8 @@ package woowacourse.kanban.board.ui.constant
 object SnackBarText {
     const val CREATE_TASK = "새로운 태스크가 추가되었습니다."
     const val STATUS_EDIT = "태스크가 이동되었습니다."
-    const val UPDATE_TASK = "새로운 태스크가 수정되었습니다."
-    const val DELETE_TASK = "새로운 태스크가 삭제되었습니다."
+    const val UPDATE_TASK = "태스크가 수정되었습니다."
+    const val DELETE_TASK = "태스크가 삭제되었습니다."
 
     const val ILLEGAL_DELETE = "해당 상태에서는 태스크 삭제가 불가합니다."
     const val ILLEGAL_STATUS_EDIT = "해당 상태로 옮길 수 없습니다."
