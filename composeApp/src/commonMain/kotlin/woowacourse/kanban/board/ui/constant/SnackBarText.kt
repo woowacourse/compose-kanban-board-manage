f18d7df6084e2f7124275c83f76c03759bf966be
ujeong <qnf323@naver.com>
2026-04-01 15:33:40 +0900

    fix: 텍스트 수정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/constant/SnackBarText.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/constant/SnackBarText.kt
@@ -4,6 +4,6 @@ object SnackBarText {
     const val CREATE_TASK = "새로운 태스크가 추가되었습니다."
 
     const val STATUS_EDIT = "태스크가 이동되었습니다."
-    const val UPDATE_TASK = "새로운 태스크가 추가되었습니다."
-    const val DELETE_TASK = "새로운 태스크가 추가되었습니다."
+    const val UPDATE_TASK = "새로운 태스크가 수정되었습니다."
+    const val DELETE_TASK = "새로운 태스크가 삭제되었습니다."
 }
