19bdace74525bb6b0227d286b45253ea29521a24
todays_sun_day <178suna@gmail.com>
2026-04-02 12:31:11 +0900

    test: UI 인터랙션 및 상태 변경 흐름 테스트 작성

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/screen/board/KanbanBoardScreenTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/screen/board/KanbanBoardScreenTest.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -78,7 +80,7 @@ class KanbanBoardScreenTest {
         onNodeWithText("새 태스크 생성").performClick()
         onNodeWithText("태스크 제목을 입력하세요.").performTextInput("안녕하세요")
         onNodeWithText("생성").performClick()
-        onNodeWithText("새로운 태스크가 추가되었습니다.").assertIsDisplayed()
+        onNodeWithText(SnackbarMessage.TASK_CREATED.text).assertIsDisplayed()
     }
 
     @Test
@@ -119,7 +121,7 @@ class KanbanBoardScreenTest {
         }
 
         // Then
-        onNodeWithText("태스크가 이동되었습니다.").assertIsDisplayed()
+        onNodeWithText(SnackbarMessage.TASK_MOVED.text).assertIsDisplayed()
     }
 
     @Test
@@ -159,7 +161,7 @@ class KanbanBoardScreenTest {
         }
 
         // Then
-        onNodeWithText("태스크가 이동되었습니다.").assertDoesNotExist()
+        onNodeWithText(SnackbarMessage.TASK_MOVED.text).assertDoesNotExist()
     }
 
     @Test
@@ -240,5 +242,134 @@ class KanbanBoardScreenTest {
         // Then: 수정할 태스크 라는 이름을 가진 태스크가 여전히 존재하고, 스낵바가 표시된다.
         onNodeWithText("수정할 태스크").assertExists()
         onNodeWithText(SnackbarMessage.TASK_EDITED.text).assertIsDisplayed()
+    }
+
+    @Test
+    fun `불가능한 상태 전이를 시도할 때 스낵바가 정상적으로 표시된다`() = runComposeUiTest {
+        // Given: To Do 상태의 태스크 한 개를 생성한다.
+        setContent {
+            CompositionLocalProvider(LocalDensity provides Density(0.1f)) {
+                KanbanBoardScreen(
+                    projects = listOf(KanbanProject("안녕")),
+                )
+            }
+        }
+        onNodeWithText("새 태스크 생성").performClick()
+        onNodeWithText("태스크 제목을 입력하세요.").performTextInput("안녕하세요")
+        onNodeWithText("다이노").performClick()
+        onNodeWithText("생성").performClick()
+        onNodeWithText("새로운 태스크가 추가되었습니다.").assertIsDisplayed()
+        onNodeWithContentDescription("닫기").performClick()
+
+        // When: To Do에 있는 태스크를 Done 으로 이동한다.
+        val taskBounds = onNodeWithText("안녕하세요")
+            .fetchSemanticsNode()
+            .boundsInRoot
+
+        val doneBounds = onNodeWithText("Done")
+            .fetchSemanticsNode()
+            .boundsInRoot
+
+        onNodeWithText("안녕하세요").performTouchInput {
+            down(center)
+            moveTo(
+                position = Offset(
+                    x = doneBounds.center.x - taskBounds.left,
+                    y = doneBounds.center.y - taskBounds.top,
+                ),
+            )
+            up()
+        }
+
+        // Then: 이동 불가능 안내 메시지가 출력된다.
+        onNodeWithText(SnackbarMessage.TASK_MOVE_NOT_ALLOWED.text).assertIsDisplayed()
+    }
+
+    @Test
+    fun `담당자를 지정하지 않고 이동을 시도할 때 스낵바가 정상적으로 표시된다`() = runComposeUiTest {
+        // Given: 담당자를 지정하지 않은 태스크 한 개를 생성한다.
+        setContent {
+            CompositionLocalProvider(LocalDensity provides Density(0.1f)) {
+                KanbanBoardScreen(
+                    projects = listOf(KanbanProject("안녕")),
+                )
+            }
+        }
+        onNodeWithText("새 태스크 생성").performClick()
+        onNodeWithText("태스크 제목을 입력하세요.").performTextInput("안녕하세요")
+        onNodeWithText("생성").performClick()
+        onNodeWithText(SnackbarMessage.TASK_CREATED.text).assertIsDisplayed()
+        onNodeWithContentDescription("닫기").performClick()
+
+        // When
+        val taskBounds = onNodeWithText("안녕하세요")
+            .fetchSemanticsNode()
+            .boundsInRoot
+
+        val doneBounds = onNodeWithText("In Progress")
+            .fetchSemanticsNode()
+            .boundsInRoot
+
+        onNodeWithText("안녕하세요").performTouchInput {
+            down(center)
+            moveTo(
+                position = Offset(
+                    x = doneBounds.center.x - taskBounds.left,
+                    y = doneBounds.center.y - taskBounds.top,
+                ),
+            )
+            up()
+        }
+
+        // Then: 담당자 지정 안내 메시지가 출력된다.
+        onNodeWithText(SnackbarMessage.TASK_ASSIGNEE_REQUIRED.text).assertIsDisplayed()
+    }
+
+    @Test
+    fun `삭제 불가능한 태스크의 삭제 버튼을 눌렀을 때 스낵바가 정상적으로 표시된다`() = runComposeUiTest {
+        // Given: Done 상태의 태스크 한 개를 생성한다.
+        setContent {
+            CompositionLocalProvider(LocalDensity provides Density(0.1f)) {
+                KanbanBoardScreen(
+                    projects = listOf(KanbanProject("안녕")),
+                )
+            }
+        }
+        onNodeWithText("새 태스크 생성").performClick()
+        onNodeWithText("태스크 제목을 입력하세요.").performTextInput("삭제할 태스크")
+        onNode(hasText("Review") and hasClickAction()).performClick()
+        onNodeWithText("다이노").performClick()
+        onNodeWithText("생성").performClick()
+
+        // When: 해당 태스크를 클릭한 후 삭제 버튼을 클릭한다.
+        onNodeWithText("삭제할 태스크").performClick()
+        onNodeWithText("삭제").performClick()
+
+        // Then: 삭제할 태스크 라는 이름을 가진 태스크가 여전히 존재하고, 스낵바가 표시된다.
+        onNodeWithText("삭제할 태스크").assertExists()
+        onNodeWithText(SnackbarMessage.TASK_DELETE_NOT_ALLOWED.text).assertIsDisplayed()
+    }
+
+    @Test
+    fun `태스크를 클릭해 태스크를 수정할 때 수정된 태스크가 화면에 잘 나타난다`() = runComposeUiTest {
+        // Given: 태스크 한 개를 생성한다.
+        setContent {
+            CompositionLocalProvider(LocalDensity provides Density(0.1f)) {
+                KanbanBoardScreen(
+                    projects = listOf(KanbanProject("안녕")),
+                )
+            }
+        }
+        onNodeWithText("새 태스크 생성").performClick()
+        onNodeWithText("태스크 제목을 입력하세요.").performTextInput("수정할 태스크")
+        onNodeWithText("생성").performClick()
+
+        // When: 해당 태스크를 클릭한 후 삭제 버튼을 클릭한다.
+        onNodeWithText("수정할 태스크").performClick()
+        onNodeWithText("태스크에 대한 자세한 설명을 입력하세요.").performTextInput("이것은 수정되었습니다.")
+        onNodeWithText("수정").performClick()
+
+        // Then: 수정할 태스크는 사라지고 수정된 태스크가 화면에 표시된다.
+        onNodeWithText("이것은 수정되었습니다.").assertExists()
     }
 }
